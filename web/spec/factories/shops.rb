a4bb59270676fb196a49e5b866f0c0a9af0cebd5
Mike <m.uribe@portal53.cl>
2024-06-14 02:10:28 -0400

    refactor

--- web/spec/factories/shops.rb
+++ web/spec/factories/shops.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :shop do
     shopify_domain { "test-shop.myshopify.com" }
-    shopify_token { "shpat_1234567890abcdef1234567890abcdef" }
+    shopify_token { "test-token-shopify" }
   end
 end
