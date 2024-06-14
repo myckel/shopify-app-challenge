f20f2ddfc1057a5741fa6226e3a5c42135f83053
Mike <m.uribe@portal53.cl>
2024-06-13 20:37:24 -0400

    update routes

---
+++ web/config/routes.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  root to: "home#index"
+
+  mount ShopifyApp::Engine, at: "/api"
+  get "/api", to: redirect(path: "/") # Needed because our engine root is /api but that breaks FE routing
+
+  # If you are adding routes outside of the /api path, remember to also add a proxy rule for
+  # them in web/frontend/vite.config.js
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  scope path: :api, format: :json do
+    resources :snapshots do
+      member do
+        post 'restore'
+      end
+    end
+
+    # POST /api/products and GET /api/products/count
+    resources :products, only: [:create, :index] do
+      collection do
+        get :count
+      end
+    end
+  end
+
+  # Any other routes will just render the react app
+  match "*path" => "home#index", via: [:get, :post]
+end
